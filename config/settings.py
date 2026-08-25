3a0d67717c37fdf157d7fc675fc361bfd2211250
Ali <alisher.abdimuminov.2005@gmail.com>
2026-08-25 21:56:06 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = "django-insecure-stnp^qh%9_1ff4*r0eyj$z534^r&538rjqx*qhb(f7)s(qfo2z
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["test.samdpi.uz", "localhost", "127.0.0.1"]
 
 
 INSTALLED_APPS = [
